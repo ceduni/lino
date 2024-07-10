71bf3a763980d3dee41fa839c4f6c61d0d1faf26
Gelehead <113441042+Gelehead@users.noreply.github.com>
2024-07-10 12:40:13 -0400

    update index html seance 10

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'widgets/SearchBar.dart';
-import 'models/bookbox_model.dart';
-import 'widgets/NavBar.dart';
 import 'Discussion.dart';
 import 'navigation.dart';
 
