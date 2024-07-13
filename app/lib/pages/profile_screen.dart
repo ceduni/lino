b9095703f8cb1d095e1ef26e32e2185a71e8e862
Hoang Quan Tran <ben.nightcorer@gmail.com>
2024-07-13 12:39:02 -0400

    refactor: Update ProfileScreen with StatisticalWidget for displaying personal and community impact statistics, and personal preferences

--- app/lib/pages/profile_screen.dart
+++ app/lib/pages/profile_screen.dart
@@ -69,7 +69,7 @@ class ProfileSectionList extends StatelessWidget {
             ),
             CustomExpansionPanel(
               headerText: 'Statistiques',
-              bodyText: 'Content of Statistiques',
+              body: StatisticalWidget(),
               isExpanded: controller.isExpanded[1],
             ),
             CustomExpansionPanel(
@@ -195,9 +195,92 @@ class BookGridContainer extends StatelessWidget {
   }
 }
 
-class AchievementWidget extends StatelessWidget {
+class StatisticalWidget extends StatelessWidget {
+  // Separate into 2 sections:
+  // Sec 1: Personal Impact, Community Impact(2 tiles)
+  // Personal Impact: Carbon savings, water saved, trees saved
+  // Community Impact: Carbon savings, water saved, trees saved
+
+  // Sec 2: Personal Preferences (3 tiles)
+  // Favorite genre, nb books borrowed, nb books given
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        GridView.count(
+          shrinkWrap: true,
+          crossAxisCount: 2,
+          children: [
+            GridTileWidget(
+              title: 'Personal Impact',
+              icon: Icons.eco,
+              navigateTo: Container(),
+            ),
+            GridTileWidget(
+              title: 'Community Impact',
+              icon: Icons.people,
+              navigateTo: Container(),
+            ),
+          ],
+        ),
+        GridView.count(
+          shrinkWrap: true,
+          crossAxisCount: 3,
+          children: [
+            GridTileWidget(
+              title: 'Favorite genre',
+              icon: Icons.book,
+              navigateTo: Container(),
+            ),
+            GridTileWidget(
+              title: 'Books borrowed',
+              icon: Icons.book,
+              navigateTo: Container(),
+            ),
+            GridTileWidget(
+              title: 'Books given',
+              icon: Icons.book,
+              navigateTo: Container(),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class GridTileWidget extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final Widget navigateTo;
+
+  const GridTileWidget({
+    Key? key,
+    required this.title,
+    required this.icon,
+    required this.navigateTo,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => navigateTo),
+        );
+      },
+      child: Card(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(icon, size: 50.0),
+            SizedBox(height: 10.0),
+            Text(title, style: TextStyle(fontSize: 20.0)),
+          ],
+        ),
+      ),
+    );
   }
 }
