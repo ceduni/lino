a8af585424ca351872c5bf1ff4e9ff29843f541a
Gelehead <113441042+Gelehead@users.noreply.github.com>
2024-07-18 10:59:33 -0400

    pls pull before committing or pushing

--- app/lib/pages/book_details_page.dart
+++ app/lib/pages/book_details_page.dart
@@ -1,25 +1,21 @@
-import 'package:Lino_app/models/book_model.dart';
 import 'package:flutter/material.dart';
-import 'package:Lino_app/services/book_services.dart';
 
 class BookDetailsPage extends StatefulWidget {
-  final String qrCode;
-  final String bookBoxId;
+  final Map<String, dynamic> book;
 
-  BookDetailsPage({required this.qrCode, required this.bookBoxId});
+  BookDetailsPage({required this.book});
 
   @override
   _BookDetailsPageState createState() => _BookDetailsPageState();
 }
 
 class _BookDetailsPageState extends State<BookDetailsPage> {
-  late Future<Map<String, dynamic>> book;
-  final bookService = BookService();
+  late Map<String, dynamic> book;
 
   @override
   void initState() {
     super.initState();
-    book = bookService.getBookFromBB(widget.qrCode, widget.bookBoxId);
+    book = widget.book;
   }
 
   @override
@@ -28,34 +24,20 @@ class _BookDetailsPageState extends State<BookDetailsPage> {
       appBar: AppBar(
         title: Text('Book Details'),
       ),
-      body: FutureBuilder<Book>(
-        future: book,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData) {
-            return Center(child: Text('Book not found'));
-          } else {
-            var book = snapshot.data!;
-            return SingleChildScrollView(
-              child: Column(
-                children: [
-                  Text(book.title, style: TextStyle(fontSize: 24)),
-                  Text('Authors: ${book.authors.join(', ')}'),
-                  Image.network(book.coverImage),
-                  Text(book.description),
-                  Text('ISBN: ${book.isbn}'),
-                  Text('Publisher: ${book.publisher}'),
-                  Text('Categories: ${book.categories.join(', ')}'),
-                  Text('Year: ${book.parutionYear}'),
-                  Text('Pages: ${book.pages}'),
-                ],
-              ),
-            );
-          }
-        },
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text(book['title'], style: TextStyle(fontSize: 24)),
+            Text('Authors: ${book['authors'].join(', ')}'),
+            Image.network(book['coverImage']),
+            Text(book['description']),
+            Text('ISBN: ${book['isbn']}'),
+            Text('Publisher: ${book['publisher']}'),
+            Text('Categories: ${book['categories'].join(', ')}'),
+            Text('Year: ${book['parutionYear']}'),
+            Text('Pages: ${book['pages']}'),
+          ],
+        ),
       ),
     );
   }
