2148214f5faf50a304c7fb9ef8bb52c841ed387a
Nathan Razafindrakoto <nathan.razafindrakoto@umontreal.ca>
2025-09-02 13:41:49 -0400

    Delete backend/dist directory

--- backend/dist/middlewares/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.superAdminAuthenticate = exports.adminAuthenticate = exports.optionalAuthenticate = exports.bookManipAuth = exports.authenticate = void 0;
-var auth_middleware_1 = require("./auth.middleware");
-Object.defineProperty(exports, "authenticate", { enumerable: true, get: function () { return auth_middleware_1.authenticate; } });
-Object.defineProperty(exports, "bookManipAuth", { enumerable: true, get: function () { return auth_middleware_1.bookManipAuth; } });
-Object.defineProperty(exports, "optionalAuthenticate", { enumerable: true, get: function () { return auth_middleware_1.optionalAuthenticate; } });
-Object.defineProperty(exports, "adminAuthenticate", { enumerable: true, get: function () { return auth_middleware_1.adminAuthenticate; } });
-Object.defineProperty(exports, "superAdminAuthenticate", { enumerable: true, get: function () { return auth_middleware_1.superAdminAuthenticate; } });
